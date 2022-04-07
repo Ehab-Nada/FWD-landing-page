e65a075dd8a8d7c9d77ea7fba0428dfa4eb4eb11
hopamnada <hopamnada@gmail.com>
2022-04-07 07:37:52 +0200

    second submition

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ function createListItems() {
   for (let section of sections) {
     //get section name from the data-nav attribute
     let secName = section.getAttribute("data-nav");
-    //get id attribute to jumb into section after clicked
-    let secLink = section.getAttribute("id");
     //create list that holds section name
     let listItem = document.createElement("li");
     //creact a element
@@ -28,29 +26,21 @@ function createListItems() {
   //append lists with anchors that create to the ul in the navbar
   document.getElementById("navbar__list").appendChild(fragment);
 }
+
 //clicking link item scroll to specific section
-function scrollToSection() {
+function linkToSection() {
   for (let item of links) {
-    item.addEventListener("click", function clicked() {
-      let active = document.getElementsByClassName("active");
-      if (active.length === 0) {
-        item.classList.add("active");
-      } else {
-        active[0].classList.remove("active");
-        item.classList.add("active");
-      }
-      for (let section of sections) {
-        if (section.getAttribute("data-nav") === item.textContent) {
-          section.scrollIntoView({
-            behavior: "smooth",
-          });
-        }
-      }
+    item.addEventListener("click", function clicked(event) {
+      event.preventDefault();
+      let sectionId = item.textContent.toLowerCase().replace(" ", "");
+      document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth",
+      });
     });
   }
 }
 
-// determine if any part of element visible in viewport
+// determine element visible in viewport
 function isInViewport(el) {
   let rect = el.getBoundingClientRect();
   return (
@@ -66,24 +56,16 @@ function isInViewport(el) {
 }
 
 //add active class to the section at the viewport
-function scrollActive() {
+function scrollToActiveSection() {
+  let active = document.getElementsByClassName("active");
   for (let section of sections) {
     //check if the section in viewport to add or remove active class
     if (isInViewport(section)) {
       section.classList.add("your-active-class");
-      let active = document.getElementsByClassName("active");
-      for (let item of links) {
-        if (item.textContent === section.getAttribute("data-nav")) {
-          if (active.length === 0) {
-            item.classList.add("active");
-          } else {
-            active[0].classList.remove("active");
-            item.classList.add("active");
-          }
-        }
-      }
+      links[section.id.replace("section", "") - 1].classList.add("active");
     } else {
       section.classList.remove("your-active-class");
+      links[section.id.replace("section", "") - 1].classList.remove("active");
     }
   }
 }
@@ -93,8 +75,8 @@ function App() {
   //run Create list function that take sections names and id to add them into li in the ul
   createListItems();
   //when user scroll the page this event listener will define which function at the view port to add active class
-  document.addEventListener("scroll", scrollActive);
-  scrollToSection();
+  document.addEventListener("scroll", scrollToActiveSection);
+  linkToSection();
 }
 
 //run the appliction
